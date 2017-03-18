ca3e97d74d943dc235fb6bb541416bf92ff73544
Shen <dustet@gmail.com>
2017-03-17 23:32:15 -0400

    init game model

--- miner/models.py
+++ miner/models.py
@@ -1,3 +1,31 @@
+import json
 from django.db import models
 
-# Create your models here.
+GameActionTypes = [
+    "CLICK",
+    "FLAG"
+]
+
+CELL_STATES = [
+    "SHOWN",
+    "HIDDEN",
+    "FLAGGED"
+]
+
+
+class Game(models.Model):
+
+    board = models.TextField()
+
+
+class GameActions(models.Model):
+
+    ACTION_TYPES = map(
+        lambda i: (i, GameActionTypes[i]),
+        range(len(GameActionTypes))
+    )
+
+    game = models.ForeignKey(Game)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    action_type = models.IntegerField(choices=ACTION_TYPES)
